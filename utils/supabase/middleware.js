83135f158320d3f265507e2a8ade188e0d5e2c71
Sıla <silakara1453@gmail.com>
2024-10-10 22:25:46 +0300

    fonksiyon içinde const tanımlandı

--- utils/supabase/middleware.js
+++ utils/supabase/middleware.js
@@ -31,6 +31,20 @@ export async function updateSession(request) {
   // supabase.auth.getUser(). A simple mistake could make it very hard to debug
   // issues with users being randomly logged out.
 
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (
+    !user &&
+    !request.nextUrl.pathname.startsWith('/login') &&
+    !request.nextUrl.pathname.startsWith('/auth')
+  ) {
+    // no user, potentially respond by redirecting the user to the login page
+    const url = request.nextUrl.clone()
+    url.pathname = '/login'
+    return NextResponse.next()
+  }
 
   // IMPORTANT: You *must* return the supabaseResponse object as it is. If you're
   // creating a new response object with NextResponse.next() make sure to:
