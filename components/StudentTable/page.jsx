3b416ddb90eb9f1434305edf21ff9a359ebee62b
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-10 20:50:15 +0300

    supabase key ve url eklendi

---
+++ components/StudentTable/page.jsx
@@ -0,0 +1,8 @@
+"use client";
+import { useState, useEffect } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+
+const supabaseUrl = "https://njrgyzhegrbsammqhhzq.supabase.co";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qcmd5emhlZ3Jic2FtbXFoaHpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzOTA2MzYsImV4cCI6MjA0Mzk2NjYzNn0.vIrnq40dsHSNRL133E863JY4ZJe9fSKOWkVewROXGcM";
+const supabase = createClient(supabaseUrl, supabaseKey);
