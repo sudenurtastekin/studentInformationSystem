abc043674d5045dd533ebde7b13edc96d5a39dca
Sudenur Taştekin <sude348tastekin@gmail.com>
2024-10-10 20:50:42 +0300

    app function oluşturuldu

--- components/StudentTable/page.jsx
+++ components/StudentTable/page.jsx
@@ -6,3 +6,7 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = "https://njrgyzhegrbsammqhhzq.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5qcmd5emhlZ3Jic2FtbXFoaHpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjgzOTA2MzYsImV4cCI6MjA0Mzk2NjYzNn0.vIrnq40dsHSNRL133E863JY4ZJe9fSKOWkVewROXGcM";
 const supabase = createClient(supabaseUrl, supabaseKey);
+
+function App(){
+  
+}
